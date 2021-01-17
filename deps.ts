f5b10dbaf233da9f4c90f8f960c5b3a9baa52a49
Jurgis Kacens <jurgis.kacens@outlook.com>
2021-01-17 10:43:31 +0200

    dep

--- deps.ts
+++ deps.ts
@@ -1,6 +1,6 @@
 export { DB } from "https://deno.land/x/sqlite/mod.ts";
-export  { sendMessage, Intents, Message } from "https://x.nest.land/Discordeno@9.0.1/mod.ts";
-import StartBot from "https://x.nest.land/Discordeno@9.0.1/mod.ts";
+export  { sendMessage, Intents, Message } from "https://x.nest.land/Discordeno@9.0.3/mod.ts";
+import StartBot from "https://x.nest.land/Discordeno@9.0.3/mod.ts";
 
 export {
     StartBot
